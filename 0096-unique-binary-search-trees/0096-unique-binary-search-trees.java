4418119cec4552f9fc68d8c2ccbee69e4e45a0c5
Aditya Mantri <adityamantri05@gmail.com>
2023-11-04 13:41:35 -0400

    Time: 0 ms (100%), Space: 39 MB (53.64%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -3,9 +3,9 @@ class Solution {
         int[] dp = new int[n+1];
         dp[0] = 1;
         dp[1] = 1;
-        for(int i = 2; i <= n; i++){
-            for(int j = 1; j <= i; j++){
-                dp[i] += dp[i-j] * dp[j-1];
+        for(int totalNodes = 2; totalNodes <= n; totalNodes++){
+            for(int currRootNode = 1; currRootNode <= totalNodes; currRootNode++){
+                dp[totalNodes] +=  dp[currRootNode-1] * dp[totalNodes-currRootNode];
             }
         }
         return dp[n];
