6c3f0d63096ad4612f0fd55cb221a9c5b43e6ae9
Aditya Mantri <adityamantri05@gmail.com>
2023-12-24 11:49:23 -0800

    Time: 5 ms, Memory: 42.4 MB - LeetHub

---
+++ permutation-in-string/permutation-in-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int len1 = s1.length(), len2 = s2.length();
+        if(len1 > len2) return false;
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        for(char c : s1.toCharArray()){
+            arr1[c-'a']++;
+        }
+        for(int i = 0; i < len2; i++){
+            char c = s2.charAt(i);
+            arr2[c-'a']++;
+            if(i >= len1){
+                arr2[s2.charAt(i-len1)-'a']--;
+            }
+            if(Arrays.equals(arr1, arr2)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
