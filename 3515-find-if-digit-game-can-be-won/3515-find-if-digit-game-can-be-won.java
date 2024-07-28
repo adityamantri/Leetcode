071ca97b466407bd138cd83ab150e74667ed26f2
Aditya Mantri <adityamantri05@gmail.com>
2024-07-27 19:33:39 -0700

    Time: 0 ms (100%), Space: 44.3 MB (100%) - LeetHub

---
+++ 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int sum = 0;
+        for(int i : nums){
+            if(i < 10){
+                sum += i;
+            }else{
+                sum -= i;
+            }
+        }
+        return sum != 0;
+    }
+}
