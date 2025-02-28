62c5ad6b09086929a5ec5202fb877f38631d67ed
Aditya Mantri <adityamantri05@gmail.com>
2025-02-28 14:31:13 -0800

    Time: 2 ms, Memory: 44.7 MB - LeetHub

--- permutations/permutations.java
+++ permutations/permutations.java
@@ -1,21 +1,24 @@
 class Solution {
-    public void recurse(int[] nums, List<List<Integer>> res, List<Integer> curr){
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        recurse(nums, 0, res, new ArrayList<>(), new HashSet<Integer>());
+        return res;
+    }
+    
+    public void recurse(int[] nums, int idx, List<List<Integer>> res, List<Integer> curr, Set<Integer> set){
         if(curr.size() == nums.length){
             res.add(new ArrayList(curr));
             return;
         }
-
+        
         for(int i = 0; i < nums.length; i++){
-            if(curr.contains(nums[i])) continue;
-            curr.add(nums[i]);
-            recurse(nums, res, curr);
-            curr.remove(curr.size()-1);
+            if(!set.contains(nums[i])){
+                set.add(nums[i]);
+                curr.add(nums[i]);
+                recurse(nums, idx+1, res, curr, set);
+                set.remove(nums[i]);
+                curr.remove(curr.size()-1);
+            }
         }
-    }
-
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        recurse(nums, res, new ArrayList<>());
-        return res;
     }
 }
