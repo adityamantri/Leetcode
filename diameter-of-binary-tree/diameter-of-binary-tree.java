a29037be94e050e2372d45700afce6398e21da54
Aditya Mantri <adityamantri05@gmail.com>
2025-02-27 22:19:25 -0800

    Time: 0 ms, Memory: 44.5 MB - LeetHub

--- diameter-of-binary-tree/diameter-of-binary-tree.java
+++ diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -16,15 +16,15 @@
 class Solution {
     int max = 0;
     public int diameterOfBinaryTree(TreeNode root) {
-        depth(root);
+        recurse(root);
         return max;
     }
     
-    public int depth(TreeNode node){
-        if(node == null) return 0;
-        int left = depth(node.left);
-        int right = depth(node.right);
-        max = Math.max(max, left+right); // we dont include +1 here for root node because it is asking for no. of EDGES, not nodes.
-        return Math.max(left, right) + 1;
+    private int recurse(TreeNode root){
+        if(root == null) return 0;
+        int left = recurse(root.left);
+        int right = recurse(root.right);
+        max = Math.max(left+right, max);
+        return 1 + Math.max(left,right);
     }
 }
