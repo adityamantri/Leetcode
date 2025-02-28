d2623e4bcb9e5b07cc5a22951d8cde03d84c68e9
Aditya Mantri <adityamantri05@gmail.com>
2025-02-27 16:44:59 -0800

    Time: 23 ms, Memory: 42.9 MB - LeetHub

--- clone-graph/clone-graph.java
+++ clone-graph/clone-graph.java
@@ -19,15 +19,15 @@ class Node {
 */
 
 class Solution {
-    Map<Node, Node> visited = new HashMap<>();
+    Map<Node, Node> map = new HashMap<>();
     public Node cloneGraph(Node node) {
         if(node == null) return null;
-        if(visited.containsKey(node)) return visited.get(node);
-        Node clone = new Node(node.val, new ArrayList());
-        visited.put(node, clone);
-        for(Node neigh : node.neighbors){
-            clone.neighbors.add(cloneGraph(neigh));
+        if(map.containsKey(node)) return map.get(node);
+        Node newNode = new Node(node.val);
+        map.put(node, newNode);
+        for(Node n : node.neighbors){
+            newNode.neighbors.add(cloneGraph(n));
         }
-        return clone;
+        return newNode;
     }
 }
