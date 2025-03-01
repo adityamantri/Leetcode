7beaa2cf4848391ecd499d2f5f29fd0d4244d303
Aditya Mantri <adityamantri05@gmail.com>
2025-02-28 20:38:40 -0800

    Time: 1 ms, Memory: 42.2 MB - LeetHub

--- subsets/subsets.java
+++ subsets/subsets.java
@@ -1,20 +1,17 @@
 class Solution {
-
-    public void backtrack(int[] nums, int idx, List<List<Integer>> res, List<Integer> curr){
-        res.add(new ArrayList<>(curr));
-        // exit condition
-        if(idx == nums.length) return;
-
-        for(int i = idx; i < nums.length; i++){
-            curr.add(nums[i]);
-            backtrack(nums, i+1, res, curr);
-            curr.remove(curr.size()-1);
-        }
-    }
-
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        backtrack(nums, 0, res, new ArrayList<>());
+        backtrack(nums, new ArrayList<>(), res, 0);
         return res;
+    }
+    
+    private void backtrack(int[] nums, List<Integer> curr, List<List<Integer>> res, int idx){
+        res.add(new ArrayList<>(curr));
+        if(curr.size() == nums.length) return;
+        for(int i = idx; i < nums.length; i++){
+            curr.add(nums[i]);
+            backtrack(nums, curr, res, i+1);
+            curr.remove(curr.size()-1);
+        }
     }
 }
