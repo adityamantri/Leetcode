30e971f483f6763d5297304ea4ebb7aa85496ac6
Aditya Mantri <adityamantri05@gmail.com>
2025-03-01 00:32:06 -0800

    Time: 0 ms, Memory: 42.4 MB - LeetHub

--- powx-n/powx-n.java
+++ powx-n/powx-n.java
@@ -1,30 +1,19 @@
 class Solution {
-    public double myPowHelper(double x, long n) {
-        if(n == 0) return 1;
+    public double myPow(double x, int n) {
         if(n < 0){
-            x = 1.0/x;
-            n = -1*n;
+            x = 1/x;
+            n = -n;
         }
         double res = 1;
         while(n != 0){
-            if((n % 2) == 1){
+            if((n % 2) == 0){
+                x *= x;
+            }else{
                 res *= x;
-                n -= 1;
+                x *= x;
             }
-            x = x*x;
             n /= 2;
         }
         return res;
     }
-    
-    public double myPow(double x, int n){
-        return myPowHelper(x, (long) n);
-    }
 }
-
-/**
-n = 10, x = 2.0
-n = 2*5
-res = 2pow(5+5) = (2+2+1 + 2+2+1)
-
-*/
